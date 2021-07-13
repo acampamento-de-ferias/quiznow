f6009d229d03cc7ec629eda39c6aa2e12ee9c94a
Gabriel Viegas da Silva <gabriel.viegas@startaideia.com.br>
2021-07-13 01:08:07 -0300

    remove min characteres de global

--- app/Controllers/RegisterController.php
+++ app/Controllers/RegisterController.php
@@ -8,10 +8,7 @@ class RegisterController extends Controller
 {
     public function index()
     {
-        echo $this->twig->render('/pages/register/index.html.twig', [
-            'usernameMinCharacteres' => USER_NAME_MIN_CHARACTERES,
-            'passwordMinCharacteres' => PASSWORD_MIN_CHARACTERES
-        ]);
+        echo $this->twig->render('/pages/register/index.html.twig');
     }
 
     public function store()
--- global.php
+++ global.php
@@ -2,5 +2,3 @@
 
 define("URL", "http://quiznow.com");
 define("PATH", __DIR__);
-define("USER_NAME_MIN_CHARACTERES", 3);
-define("PASSWORD_MIN_CHARACTERES", 3);
