8ad4952778e8928e293d8050733ff429dfdc59aa
Gabriel Viegas <gabrielgl13@hotmail.com>
2021-10-10 23:58:29 -0300

    finaliza tela de create quiz para mobile

--- public/js/pages/create-quiz.js
+++ public/js/pages/create-quiz.js
@@ -10,3 +10,25 @@ function selectCategory(className) {
     element.classList.add("active-option");
     categorySelected = element;
 }
+
+const urlArrowDown = "./images/arrow-collapse-down.png";
+const urlArrowRight = "./images/arrow-collapse-right.png";
+function selectAccordion(event) {
+    const element = event.children[0];
+
+    if (element.classList.contains("arrow-down")) {
+        element.setAttribute("src", urlArrowRight);
+        element.setAttribute("alt", "Seta para direita");
+        element.classList.remove('arrow-down');
+        element.classList.add("arrow-right");
+        return;
+    }
+
+    if (element.classList.contains("arrow-right")) {
+        element.setAttribute("src", urlArrowDown);
+        element.setAttribute("alt", "Seta para baixo");
+        element.classList.remove('arrow-right');
+        element.classList.add("arrow-down");
+        return;
+    }
+}
