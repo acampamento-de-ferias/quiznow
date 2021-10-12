cf44ee0708454b2ffdb86b0f042d5ac0c5483860
Gabriel Viegas <gabrielgl13@hotmail.com>
2021-10-11 21:32:26 -0300

    fix bug discovered by tais

--- public/js/pages/create-quiz.js
+++ public/js/pages/create-quiz.js
@@ -15,6 +15,7 @@ const urlArrowDown = "./images/arrow-collapse-down.png";
 const urlArrowRight = "./images/arrow-collapse-right.png";
 function selectAccordion(event) {
     const element = event.children[0];
+    const elementButton = event;
 
     if (element.classList.contains("arrow-down")) {
         element.setAttribute("src", urlArrowRight);
@@ -29,6 +30,15 @@ function selectAccordion(event) {
         element.setAttribute("alt", "Seta para baixo");
         element.classList.remove('arrow-right');
         element.classList.add("arrow-down");
+        Array.from(document.getElementsByClassName("accordion-button")).forEach(elementF => {
+            if (elementF.getAttribute("id") !== elementButton.getAttribute("id")
+            && elementF.children[0].classList.contains("arrow-down")) {
+                elementF.children[0].setAttribute("src", urlArrowRight);
+                elementF.children[0].setAttribute("alt", "Seta para direita");
+                elementF.children[0].classList.remove('arrow-down');
+                elementF.children[0].classList.add("arrow-right");
+            }
+        });
         return;
     }
 }
